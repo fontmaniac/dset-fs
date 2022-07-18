49a189f61deb0a781ccbd9abb78442895d5b275b
Alexey Kosobrukhov <fontmaniac@users.noreply.github.com>
2022-07-18 15:06:21 +1000

    Started F#-reimplementation

---
+++ EasyMorphFs/Data.fs
@@ -0,0 +1,127 @@
+﻿namespace EasyMorphFs
+
+open Microsoft.FSharp.Core
+
+module Data =
+
+    type CellType = 
+    | Number = 0
+    | Text = 1
+    | Boolean = 2
+    | Nothing = 3
+    | Error = 4
+
+    type Nothing = Unit
+
+    type VocabularyItem = {
+        Type : CellType
+        Index : int
+    }
+
+    type IAccess =
+        abstract member GetError : i:int -> option<string>
+        abstract member GetText : i:int -> option<string>
+        abstract member GetNumber : i:int -> option<decimal>
+        abstract member GetBoolean : i:int -> option<bool>
+        abstract member GetNothing : i:int -> option<Nothing>
+        abstract member GetType : i:int -> CellType
+
+    type IColumn = 
+        inherit IAccess
+        abstract member Name : string with get
+        abstract member Length : int with get
+
+    type IDataset = 
+        abstract member Name : string with get
+        abstract member Columns : IColumn[] with get
+        abstract member ColumnsCount : int with get
+        abstract member RowsCount : int with get
+        abstract member GetRow : rowIndex:int -> IAccess
+    
+    type Vocabulary (strings : string[], decimals : decimal[], items : VocabularyItem[]) = 
+        let get i cellType make = 
+            let item = items.[i]
+            if item.Type = cellType then Some(make item.Index) else None
+
+        interface IAccess with
+            member this.GetError i      = get i CellType.Error   (fun idx -> strings.[idx])
+            member this.GetText i       = get i CellType.Text    (fun idx -> strings.[idx])
+            member this.GetNumber i     = get i CellType.Number  (fun idx -> decimals.[idx])
+            member this.GetBoolean i    = get i CellType.Boolean (fun idx -> idx = 1)
+            member this.GetNothing i    = get i CellType.Nothing (fun _ -> ())
+            member this.GetType i       = items.[i].Type
+
+    type ConstantColumn (length, constant, type', name) = 
+        let get cellType make = 
+            if type' = cellType then Some(make ()) else None
+
+        interface IColumn with
+            member this.Name = name
+            member this.Length = length
+            member this.GetError i      = get CellType.Error   (fun () -> unbox<string> constant)
+            member this.GetText i       = get CellType.Text    (fun () -> unbox<string> constant)
+            member this.GetNumber i     = get CellType.Number  (fun () -> unbox<decimal> constant)
+            member this.GetBoolean i    = get CellType.Boolean (fun () -> unbox<bool> constant)
+            member this.GetNothing i    = get CellType.Nothing (fun () -> unbox<Nothing> constant)
+            member this.GetType i       = type'
+
+    type CompressedColumn (vectorBytes:byte[], vocabulary:IAccess, bitWidth, length, name) = 
+        let _mask = (1UL <<< bitWidth) - 1UL;
+        let getVocabularyIndex rowIndex =
+            //calculate firstBut position in byte array
+            let firstBit = (uint64 rowIndex) * (uint64 bitWidth)
+            //byte index where first bit is located
+            let byteIndex = int (firstBit / 8UL)
+            //Bit number in first byte
+            let byteOffset = int (firstBit % 8UL)
+            //Move first bit to 0 position and store to result
+            let result = uint64 (vectorBytes.[int byteIndex] >>> byteOffset)
+            //calculate number of read bits
+            let bitsRead = 8 - byteOffset;
+
+            let rec read byteIndex bitsRead result =
+                //while need to read more bits
+                if bitsRead >= bitWidth then result
+                else
+                    //increase byte index
+                    let byteIndex = byteIndex + 1;
+                    //store new byte in result
+                    let result = uint64 (vectorBytes.[byteIndex]) <<< bitsRead ||| result
+                    read byteIndex (bitsRead + 8) result
+
+            //calc result and trim with bit mask
+            read byteIndex bitsRead result 
+            |> fun x -> x &&& _mask
+            |> int
+            
+        interface IColumn with
+            member this.Name = name
+            member this.Length = length
+            member this.GetError i      = i |> getVocabularyIndex |> vocabulary.GetError
+            member this.GetText i       = i |> getVocabularyIndex |> vocabulary.GetText
+            member this.GetNumber i     = i |> getVocabularyIndex |> vocabulary.GetNumber
+            member this.GetBoolean i    = i |> getVocabularyIndex |> vocabulary.GetBoolean
+            member this.GetNothing i    = i |> getVocabularyIndex |> vocabulary.GetNothing
+            member this.GetType i       = i |> getVocabularyIndex |> vocabulary.GetType
+
+    type Dataset (columns, name) = 
+        interface IDataset with
+            member this.Name = name
+            member this.Columns = columns
+            member this.ColumnsCount = columns.Length
+            member this.RowsCount = if columns.Length > 0 then columns.[0].Length else 0
+            member this.GetRow rowIndex =
+                { new IAccess with 
+                    member _.GetError i      = columns.[i].GetError(rowIndex)
+                    member _.GetText i       = columns.[i].GetText(rowIndex)
+                    member _.GetNumber i     = columns.[i].GetNumber(rowIndex)
+                    member _.GetBoolean i    = columns.[i].GetBoolean(rowIndex)
+                    member _.GetNothing i    = columns.[i].GetNothing(rowIndex)
+                    member _.GetType i       = columns.[i].GetType(rowIndex)
+                }
+
+
+
+
+
+        
---
+++ EasyMorphFs/Drivers.fs
@@ -0,0 +1,127 @@
+﻿namespace EasyMorphFs
+
+open Microsoft.FSharp.Core
+open Data
+open System
+open System.Threading
+open System.Threading.Tasks
+open System.IO
+
+module Drivers =
+
+    type SymbolType = 
+    | Nothing = 0uy
+    | Int8 = 1uy
+    | Int16 = 2uy
+    | Int32 = 4uy
+    | Decimal = 16uy
+    | Text = 32uy
+    | BoolTrue = 64uy
+    | BoolFalse = 65uy
+    | Error = 128uy
+
+    type ImportConfig = {
+        FileName : string
+        Token : CancellationToken
+    }
+    with 
+        static member Make fileName = { FileName = fileName; Token = CancellationToken.None }
+
+    type IImportDriver = 
+        abstract member ImportAsync : config:ImportConfig -> Task<IDataset[]>
+
+    type ImportDriver () as me =
+
+        //Version of dset file
+        let VERSION00 = "HIBC00"
+        let VERSION01 = "HIBC01"
+        //Constant which defines compressed type of column in file
+        let COMPRESSED_COLUMN_TYPE_NAME = "Column.Compressed"
+        //Constant which defines constant type of column in file
+        let CONSTANT_COLUMN_TYPE_NAME = "Column.Constant"
+        //Item types
+        let FIELD_ITEM_TYPE = "EasyMorph.CompressedField.0"
+        let TABLE_METADATA_ITEM_TYPE = "EasyMorph.TableMetadata.0"
+        let COLUMNS_METADATA_ITEM_TYPE = "EasyMorph.ColumnsMetadata.0"
+
+
+        let readCell (br:BinaryReader) =
+            match br.ReadByte () |> LanguagePrimitives.EnumOfValue with
+            | SymbolType.Nothing -> CellType.Nothing, box ()
+            | SymbolType.Int8 -> CellType.Number, box (decimal (br.ReadSByte ()))
+            | SymbolType.Int16 -> CellType.Number, box (decimal (br.ReadInt16 ()))
+            | SymbolType.Int32 -> CellType.Number, box (decimal (br.ReadInt32 ()))
+            | SymbolType.Decimal -> CellType.Number, box (br.ReadDecimal ())
+            | SymbolType.Text -> CellType.Text, box (br.ReadString ())
+            | SymbolType.BoolTrue -> CellType.Boolean, box (true)
+            | SymbolType.BoolFalse -> CellType.Boolean, box (false)
+            | SymbolType.Error -> 
+                br.ReadInt32 () |> ignore    //Error code. Not used as of now. Reserved for future versions.
+                CellType.Error, box (br.ReadString ())
+            | x ->
+                raise (Exception(sprintf "Unknown symbol type: %A" x))
+
+        let readCompressedColumn (br:BinaryReader) fieldName (token:CancellationToken) = 
+            async {
+                token.ThrowIfCancellationRequested ()
+
+                let vocabularyLength = br.ReadInt32 ()
+                return 0
+            }
+
+        interface IImportDriver with
+            member this.ImportAsync config = 
+                async {
+                    // Validate input
+                    if String.IsNullOrWhiteSpace config.FileName then
+                        raise (ArgumentException ("Should be not empty", nameof(config.FileName)))
+                
+                    if File.Exists config.FileName |> not then
+                        raise (FileNotFoundException("File not found.", config.FileName))
+
+                    // Non-idiomatic for F#. Need to rethink.
+                    let columns = System.Collections.Generic.List<IColumn>()
+                    let mutable tableName = ""
+
+                    use fs = new FileStream(config.FileName, FileMode.Open, FileAccess.Read, FileShare.Read)
+                    use br = new BinaryReader(fs)
+
+                    //Read version from file
+                    let version = string(br.ReadChars(6));
+
+                    if not (version.Equals(VERSION00)) && not (version.Equals(VERSION01)) then
+                        raise (Exception("Wrong file header format."))
+
+                    while fs.Position < fs.Length do 
+                        config.Token.ThrowIfCancellationRequested ()
+
+                    // Item name
+                    tableName <- br.ReadString ()
+                    // Item type
+                    let itemType = br.ReadString ()
+
+                    //Length of current block(field)
+                    let blockLength = if version.Equals(VERSION00) then int64 (br.ReadInt32 ()) else br.ReadInt64 ();
+                    let blockStartPosition = fs.Position;
+
+                    // Only non-encrypted field items are supported
+                    if itemType = FIELD_ITEM_TYPE then
+                        ignore()
+                        //let! column = ReadColumn(br, config.Token);
+
+                        //Add columns to result
+                        //columns.Add(column);
+
+                    // Metadata items are ignored
+                    elif (itemType <> TABLE_METADATA_ITEM_TYPE) && (itemType <> COLUMNS_METADATA_ITEM_TYPE) then
+                        raise (Exception(sprintf "Unsupported item type: %A" itemType));
+
+                    //Seek to end of block
+                    fs.Seek(blockStartPosition + blockLength, SeekOrigin.Begin) |> ignore;
+
+                    return Array.empty<IDataset>
+                } |> Async.StartAsTask
+
+
+
+
